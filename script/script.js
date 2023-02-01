89afe69ba0b91e459e97b4611b0821e854072fb1
Techniczeus <100887438+Techniczeus@users.noreply.github.com>
2023-02-01 18:04:01 -0300

    adiciona keyframe flutuante e implementa scroll suave

--- script/script.js
+++ script/script.js
@@ -1,10 +1,18 @@
 const menuButton = document.getElementById('menu-btn')
-const languages = document.querySelectorAll('.languages')
 
 let menuBtn = function() {
     let navBar = document.getElementById('collapse')
     navBar.classList.toggle('setCollapse')
-    
 }
-
 menuButton.addEventListener('click', menuBtn)
+
+// Smooth scroll
+
+const links = document.querySelectorAll('.nav-link');
+links.forEach(item =>{
+    item.addEventListener('click', () => {
+        const element = document.getElementById(item.getAttribute("data-link"));
+        element.scrollIntoView({behavior:'smooth'})
+    })
+    
+})
